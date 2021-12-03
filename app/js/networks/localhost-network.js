7a1854674eed4db90d763bf9b361e90bafdbe728
ingig <ingigauti@gmail.com>
2021-12-03 19:21:27 +0000

    continue with better deploy

--- app/js/networks/localhost-network.js
+++ app/js/networks/localhost-network.js
@@ -1,9 +1,11 @@
-//localhost
 
-export const localhostNetwork = function() {
-	this.ServerUrl = "https://xcqqzykqhjwv.usemoralis.com:2053/server";
-	this.AppId = "XP6fMmUXTiAH4yYBeigdjtkTmVOKhrTqdguMTE88";
-	this.ChainId = 31337;
-	this.Name = "localhost";
+//localhost
+export class localhostNetwork {
+	constructor() {
+		this.ServerUrl = "https://17nhgnogptv1.usemoralis.com:2053/server";
+		this.AppId = "4dRXtAbVr8R3vHyCYKJBrYujd2IdxYqrGbe7ePAy";
+		this.ChainId = 31337;
+		this.Name = "localhost";
+	}
 }
 
--- app/js/networks/rinkeby-network.js
+++ app/js/networks/rinkeby-network.js
@@ -1,8 +1,8 @@
 
 
-export const rinkebyNetwork = () => {
+export function rinkebyNetwork() {
 	ServerUrl = "https://rucsd2xip9xc.usemoralis.com:2053/server";
-  	AppId = "WrszROWRp7oShP39MWHMLl4mMA6n2QMN8LDRD6gi";
-  	ChainId = 4;
+	AppId = "WrszROWRp7oShP39MWHMLl4mMA6n2QMN8LDRD6gi";
+	ChainId = 4;
 	Name = "rinkeby";
 }
