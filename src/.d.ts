fd89060f7815152fd086b0850d4c8ad496db5e1c
ingig <ingigauti@gmail.com>
2022-09-28 13:46:03 +0000

    fix return

--- src/.d.ts
+++ src/.d.ts
@@ -1,5 +1,6 @@
 // In a `.d.ts` file within the included folders of your project
 import Moralis from 'Moralis';
+import Handlebars from "handlebars";
 
 declare global {
     export const Moralis : Moralis;
