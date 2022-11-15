2a5ccf08bed9521bbc6124236eee0a1734aec071
ingig <ingigauti@gmail.com>
2022-11-15 20:11:59 +0000

    change order of serverurl

--- src/networks/TestNetwork.ts
+++ src/networks/TestNetwork.ts
@@ -5,12 +5,12 @@ export default class TestNetwork extends Network {
     constructor() {
         super();
 
-        if (window.location.host.indexOf('localhost') != -1) {
-            this.ServerUrl = "https://pkkenhl7syns.grandmoralis.com:2053/server";
-            this.AppId = "QpShD4VYQT6N7evc2vMu3VtEKSEGjQPSbuJNhlIq";
-        } else {
+        if (window.location.host.indexOf('localhost') == -1) {
             this.ServerUrl = "https://f8t1vrrwtboa.usemoralis.com:2053/server";
             this.AppId = "XZhp3wQobrKiCib0Bf4FPVKAUhbHM9SvTLKOKvBb";
+        } else {
+            this.ServerUrl = "https://pkkenhl7syns.grandmoralis.com:2053/server";
+            this.AppId = "QpShD4VYQT6N7evc2vMu3VtEKSEGjQPSbuJNhlIq";
         }
         this.TestNetwork = true;
     }
