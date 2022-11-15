0f84ebb1281103f71be6c2d3ee4c2defd6822321
ingig <ingigauti@gmail.com>
2022-11-15 17:31:33 +0000

    change server

--- src/networks/MainNetwork.ts
+++ src/networks/MainNetwork.ts
@@ -5,8 +5,8 @@ export default class MainNetwork extends Network {
     constructor() {
         super();
 
-        this.ServerUrl = "https://03sj8bj43s54.grandmoralis.com:2053/server";
-        this.AppId = "G62wwf2lVoNz0v5tKRZKHPcHWjEh2Xd816UyRQYU";
+        this.ServerUrl = "https://rokinwgcthqy.grandmoralis.com:2053/server";
+        this.AppId = "FqJxfp9xng1SbovOToR2fnFjnBJju2Ko67nmTfFF";
         this.TestNetwork = false;
     }
 }
