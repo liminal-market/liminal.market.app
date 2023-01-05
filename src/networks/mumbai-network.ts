1b012451628997a736a2e6d6becef29ff882edaa
ingig <ingigauti@gmail.com>
2023-01-05 19:49:19 +0000

    debug signMessage issue

--- src/networks/mumbai-network.ts
+++ src/networks/mumbai-network.ts
@@ -1,5 +1,3 @@
-
-
 import Network from './Network';
 import TestNetwork from "./TestNetwork";
 
@@ -13,12 +11,11 @@ export default class mumbaiNetwork extends TestNetwork {
         this.NativeCurrencyName = "Matic";
         this.NativeSymbol = "MATIC";
         this.NativeDecimal = 18;
-        this.RpcUrl = 'https://polygon-mumbai.g.alchemy.com/v2/FILTBk8FT9RzcRVxkMXIg7Ipl8JCNdK2';
+        this.RpcUrl = 'https://rpc-mumbai.matic.today/';
         this.BlockExplorer = 'https://mumbai.polygonscan.com/';
-            this.FaucetUrl = 'https://faucet.polygon.technology/';
-            this.BuyUrl = 'https://www.moonpay.com/buy/matic';
-	}
-
+        this.FaucetUrl = 'https://faucet.polygon.technology/';
+        this.BuyUrl = 'https://www.moonpay.com/buy/matic';
+    }
 
 
 }
