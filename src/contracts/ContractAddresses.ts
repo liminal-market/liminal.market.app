27bd9308dfec755df403d16c2d38d5442c65e522
Ingi Gauti <ingigauti@gmail.com>
2023-02-20 21:12:20 +0000

    Service contract (#22)
    * update contract info
    
    * update contract info
    
    * test
    
    * revert test

--- src/contracts/ContractAddresses.ts
+++ src/contracts/ContractAddresses.ts
@@ -4,4 +4,6 @@ export default abstract class ContractAddresses {
     KYC_ADDRESS = "";
     AUSD_ADDRESS = "";
     LIMINAL_MARKET_ADDRESS = "";
+    LM_ADDRESS = "";
+    SERVICE_CONTRACT_ADDRESS = "";
 }
--- src/networks/mumbai-network.ts
+++ src/networks/mumbai-network.ts
@@ -11,7 +11,7 @@ export default class mumbaiNetwork extends TestNetwork {
         this.NativeCurrencyName = "Matic";
         this.NativeSymbol = "MATIC";
         this.NativeDecimal = 18;
-        this.RpcUrl = 'https://rpc-mumbai.matic.today/';
+        this.RpcUrl = 'https://polygon-mumbai.g.alchemy.com/v2/FILTBk8FT9RzcRVxkMXIg7Ipl8JCNdK2';
         this.BlockExplorer = 'https://mumbai.polygonscan.com/';
         this.FaucetUrl = 'https://faucet.polygon.technology/';
         this.BuyUrl = 'https://www.moonpay.com/buy/matic';
